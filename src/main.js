02dcbd7170202743279ff28ecff0da2f279dbda3
kyrenieee <kcestocapio05@gmail.com>
2026-06-19 10:07:39 +0800

    added vue files for designated features

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWVm1dTAa0Uu_e3_AIw6dh0F5b4fEFhmc",
+  authDomain: "autocert-38ccd.firebaseapp.com",
+  projectId: "autocert-38ccd",
+  storageBucket: "autocert-38ccd.firebasestorage.app",
+  messagingSenderId: "278595522697",
+  appId: "1:278595522697:web:152576d7c6de71b0c33658",
+  measurementId: "G-E0N0GVFRJL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
