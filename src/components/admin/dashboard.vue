0128e96ac347987cad8310b76ab6d6a3caf3d7fa
zairen <lapidzairen@gmail.com>
2026-06-24 16:59:53 +0800

    fix dashboard

--- src/components/admin/dashboard.vue
+++ src/components/admin/dashboard.vue
@@ -9,15 +9,17 @@ export default {
 </script>
 
 <template>
-    <div class="flex flex-col">
-        <div class="">
-
+    <section class="flex flex-col bg-[#1C2D27] p-4 gap-4">
+        <div class="justify-items-end">
+            <RouterLink to="/eventcal" class="hover:bg-white/15active:bg-white/30 text-white px-4 py-2 rounded-4xl">Schedule New Event  
+            </RouterLink>
+     </div>
+        <div class="flex justify-between gap-4">
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
+            <div class=""></div>
         </div>
-        <div class="">
-
-        </div>
-    </div>
+    </section>
 </template>
-
-
 
